0f8b567705cad17a338c068e97cc139617b60fb7
Ken Snyder <ken@ken.net>
2022-12-09 14:19:07 -0800

    chore: added in builder APIs

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import Components from 'unplugin-vue-components/vite'
 import Inspect from 'vite-plugin-inspect'
 import Layouts from 'vite-plugin-vue-layouts'
 import Markdown from 'vite-plugin-md'
-// import code from '@yankeeinlondon/code-builder'
-// import link from '@yankeeinlondon/link-builder'
-// import meta from '@yankeeinlondon/meta-builder'
+import code from '@yankeeinlondon/code-builder'
+import link from '@yankeeinlondon/link-builder'
+import meta from '@yankeeinlondon/meta-builder'
 import Pages from 'vite-plugin-pages'
 import Unocss from 'unocss/vite'
 import Vue from '@vitejs/plugin-vue'
@@ -60,13 +60,13 @@ const config = defineConfig({
       style: {
         baseStyle: 'github',
       },
-      // builders: [
-      //   meta(),
-      //   link(),
-      //   code({
-      //     theme: 'base',
-      //   }),
-      // ],
+      builders: [
+        meta(),
+        link(),
+        code({
+          theme: 'base',
+        }),
+      ],
     }),
 
     Inspect(),
